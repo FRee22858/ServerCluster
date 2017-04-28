549f597c105cdb665d02159ce04d4b086bd3cd20
FRee22858 <419610242@qq.com>
2017-04-28 22:28:37 +0800

    update
    update

---
+++ ServerLibrarys/ServerShared/AbstractServer.cs
@@ -0,0 +1,33 @@
+﻿namespace ServerShared
+{
+    public enum Mode
+    {
+        /// <summary>
+        /// 手动
+        /// </summary>
+        Manual =0,
+        /// <summary>
+        /// 自动
+        /// </summary>
+        Auto =1,
+    }
+    public abstract class AbstractServer
+    {
+        private Mode startMode;
+        public Mode StartMode
+        {
+            get { return startMode; }
+            set { startMode = value; }
+        }
+        private string serverName;
+        public string ServerName
+        {
+            get { return serverName; }
+            set { serverName = value; }
+        }
+        public abstract void Init(string[] args);
+        public abstract  void Exit();
+        public abstract void Run();
+        public abstract void ProcessInput();
+    }
+}
--- Servers/GlobalServer/Program.cs
+++ Servers/GlobalServer/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GlobalServerLib.Server;
+using Logger;
+using System.Threading;
+using ServerShared;
 
 namespace GlobalServer
 {
@@ -10,6 +10,33 @@ namespace GlobalServer
     {
         static void Main(string[] args)
         {
+            Api api = new Api();
+            try
+            {
+                api.ServerName = "GlobalServer";
+                api.StartMode = Mode.Auto;
+                api.Init(args);
+            }
+            catch (Exception e)
+            {
+                LOG.Error("Global init failed:{0}", e.ToString());
+                api.Exit();
+                return;
+            }
+
+            Thread thread = new Thread(api.Run);
+            thread.Start();
+
+            LOG.Info("Global Server OnReady..");
+
+            while (thread.IsAlive)
+            {
+                api.ProcessInput();
+                Thread.Sleep(1000);
+            }
+
+            api.Exit();
+            LOG.Info("Global Server Exit..");
         }
     }
 }
