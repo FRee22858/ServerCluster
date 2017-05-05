e814e0cce8ea201edc98781375eb485af2b59b52
FRee22858 <419610242@qq.com>
2017-05-05 18:09:46 +0800

    update server api

---
+++ Servers/BattleServerLib/Server/Api.cs
@@ -0,0 +1,27 @@
+﻿using ServerShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleServerLib.Server
+{
+    public class Api : AbstractServer
+    {
+        public override void InitModule(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
