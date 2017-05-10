0b347ea00776591ef2030386288ea146406331a5
Frank <419610242@qq.com>
2017-05-10 18:49:56 +0800

    update

---
+++ CommonLibrarys/LibTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using CommonUtility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        void InitData()
+        {
+            string[] files = Directory.GetFiles(PathMng.FullPathFromData("XML"), "*.xml", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                
+            }
+
+        }
+    }
+}
